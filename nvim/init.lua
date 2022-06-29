6dca82af7630b13e5929ac097da9dccf0f66dfd4
giluis <lupevibogil@gmail.com>
2022-06-30 00:29:33 +0100

    fix(keymaps): solved mapping issue
    The issue was the `packer_init.lua` was being sourced before `editor.lua` in `init.lua`. This caused `telescopeconfig.lua` to be sourced before the `leader>` was set in `editor.lua`. The issue was solved by sourcing the files in the correct order
    The issue affecting lsp was that diagnostics were being mapped before
    on_attach function
    Signed-off-by: giluis <lupevibogil@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,6 @@
 require('impatient')
-require('packer_init')
--- require('plugins.telescopeconfig')
 require('editor')
 require('disabled')
-require('plugins.lsp')
-require('plugins.autosave')
-require('plugins.terminal')
-require('plugins.treesitter')
-require('plugins.nvimtree')
-require('plugins.lightspeed')
-require('plugins.devicons')
-require('plugins.replace')
-require('plugins.cmpconfig')
-
+require('packer_init')
 
 
---
+++ nvim/lua/plugins/telescopeconfig.lua
@@ -0,0 +1,19 @@
+local actions = require("telescope.actions")
+
+-- Telescope
+vim.api.nvim_set_keymap('n','<leader>ff' ,'<cmd>lua require("telescope.builtin").find_files()<cr>',{ noremap= true})
+vim.api.nvim_set_keymap('n','<leader>fg' ,'<cmd>lua require("telescope.builtin").live_grep()<cr>',{ noremap= true})
+vim.api.nvim_set_keymap('n','<leader>fb' ,'<cmd>lua require("telescope.builtin").buffers()<cr>',{ noremap= true})
+vim.api.nvim_set_keymap('n','<leader>fh' ,'<cmd>lua require("telescope.builtin").help_tags()<cr>',{ noremap= true})
+
+
+require('telescope').setup{
+    -- defaults = {
+    --     file_ignore_patterns = {"node_modules", "target"},
+    --     mappings = {
+    --         i = {
+    --             ["<M-v>"] = actions.file_vsplit
+    --         }
+    --     }
+    -- }
+}
