12b50fde840f17ce8eef119d32d702e8feccc356
giluis <lupevibogil@gmail.com>
2022-07-03 18:23:24 +0100

    fix(ui): removed ruler and showmode
    Removed ruler and show mode from the status line (might add showmode
    later, just trying it out)
    Removed two useless commands (edit .zshrc and init.lua) since they are
    better called from the command line

--- nvim/lua/editor.lua
+++ nvim/lua/editor.lua
@@ -1,18 +1,17 @@
-require('util')
-
 -- mapleader
 vim.g.mapleader='ç'
 
 -- General
-noremap("n","<leader>v","<C-v>")
-noremap("n","<C-q>","<C-w>q")
-noremap("n","<C-k>","<C-w>k")
-noremap("n","<C-h>","<C-w>h")
-noremap("n","<C-j>","<C-w>j")
-noremap("n","<C-l>","<C-w>l")
-noremap("n","<leader>ec",":vsplit $HOME/.config/.zshrc<CR>")
-noremap("n","<leader>ev",":vsplit $HOME/.config/nvim/init.vim<CR>")
-noremap("n","<leader>n"," :NvimTreeToggle<CR>")
+local keymap = vim.api.nvim_set_keymap
+keymap('n',"<leader>v","<C-v>",{ noremap= true})
+keymap('n',"<C-q>","<C-w>q",{ noremap= true})
+keymap('n',"<C-k>","<C-w>k",{ noremap= true})
+keymap('n',"<C-h>","<C-w>h",{ noremap= true})
+keymap('n',"<C-j>","<C-w>j",{ noremap= true})
+keymap('n',"<C-l>","<C-w>l",{ noremap= true})
+keymap('n',"<leader>ec",":vsplit $HOME/.config/.zshrc<CR>",{ noremap= true})
+keymap('n',"<leader>ev",":vsplit $HOME/.config/nvim/init.vim<CR>",{ noremap= true})
+keymap('n',"<leader>n"," :NvimTreeToggle<CR>",{ noremap= true})
 
 
 -- Editor Options
@@ -30,10 +29,12 @@ opt.expandtab=true
 opt.shiftwidth=4
 opt.autoindent=true
 opt.scrolloff=5
+opt.ruler = false
 
 opt.relativenumber = true
 opt.number = true
-opt.signcolumn ="yes"
+opt.signcolumn = "yes"
+opt.showmode = false
 
 vim.cmd('colorscheme tokyonight')
 
